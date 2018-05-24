2e9e3b4b6e9262c6cabe5e142f58bac1e5972d99
Nicolas <nicolas.kempf@insa-rouen.fr>
2018-05-24 16:04:07 +0200

    Test

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,43 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=0">
-    <!--ne passe pas la validation-->
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    <link href="bootstrap-4.1.0-dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
-
-    <title>INSA Toolkit</title>
-    <link rel="icon" href="ressources/gobelet.ico">
-</head>
-
-<body>
-
-    <nav class="navbar navbar-fixed-top navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">
-            <img src="ressources/gobelet_bleu.png" width="30" class="d-inline-block align-top" alt=""> INSA Toolkit
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Commander</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
     <div class="container side-collapse-container" id="contenu">
         <div class="row justify-content-center">
             <div class="col-md-6">
@@ -67,18 +30,4 @@
         </div>
 
     </div>
-
-    <footer>
-
-        <script src="bootstrap-4.1.0-dist/js/bootstrap.js"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-
-
-        <script src="navbars.js"></script>
-        <script src="form_connect.js"></script>
-
-    </footer>
-
-</body>
-
-</html>
+@endsection
