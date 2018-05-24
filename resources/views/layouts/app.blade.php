d0e466b8cb6968ec932323890bba7fe54db31fda
Peltier Benjamin <benjamin.peltier@insa-rouen.fr>
2018-05-24 16:00:22 +0200

    Ajout du style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"> 
+    <link href="style.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
